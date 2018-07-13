5a77cbb923c47f2ac0f537e26050c33dcbae07b2
Devin Mounts <devinmounts@gmail.com>
2018-07-13 09:08:59 -0700

    setters and getter for name and description pass.

--- HairSalon.Tests/ModelTests/StylistTests.cs
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -12,12 +12,29 @@ namespace HairSalon.Tests
         {
             DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=devin_mounts_test;";
         }
+
         [TestMethod]
         public void GetSetId_SetsGetsId_Int()
         {
             Stylist newStylist = new Stylist(1, "Brandy", "Works Monday");
 
             Assert.AreEqual(1, newStylist.GetId());
+        }
+
+        [TestMethod]
+        public void GetSetName_SetsGetsName_String()
+        {
+            Stylist newStylist = new Stylist(1, "Brandy", "Works Monday");
+
+            Assert.AreEqual("Brandy", newStylist.GetName());
+        }
+
+        [TestMethod]
+        public void GetSetDescription_SetsGetsDescription_String()
+        {
+            Stylist newStylist = new Stylist(1, "Brandy", "Works Monday");
+
+            Assert.AreEqual("Works Monday", newStylist.GetDescription());
         }
     }
 }
